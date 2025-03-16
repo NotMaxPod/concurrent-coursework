ad5cc743ce37f196026981b1fef5e9739dd221be
NotMaxPod <podolnymaxim@gmail.com>
2025-03-16 14:51:10 +0100

    basic encryption

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 # Python program to implement client side of chat room. 
-import socket 
+
+from cryptography.fernet import Fernet
 import threading
+import socket 
+import bcrypt
  
-def main():
+def sendRequest():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '127.0.0.1'
     port = 12344
@@ -15,5 +18,37 @@ def main():
         response = data.decode('utf-8')
         print(f"Server response: {response}")
 
+def makeKey():
+    return Fernet.generate_key()
+
+def encryptData(key, message):
+    encoding = Fernet(key)
+    return encoding.encrypt(message.encode())
+
+def decodeData(key, message):
+    encoding = Fernet(key)
+    return encoding.decrypt(message).decode()
+
+def hashPass(password):
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt)
+
+def verifyPass(password, hashed_password):
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
+def registerNewUser():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    key = makeKey()
+
+    secureName = encryptData(key, username)
+    securePass = hashPass(password)
+    return secureName, securePass, key
+
+def loginUser(username, password, key):
+    if username == decodeData(key, username):
+        if verifyPass(password, password):
+            ...
+
 if __name__ == "__main__":
-    main()
+    sendRequest()
