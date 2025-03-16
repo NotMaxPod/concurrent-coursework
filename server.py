2e575ef1ed1b0bd5b36b8aff5f3d22988435f117
NotMaxPod <podolnymaxim@gmail.com>
2025-03-16 14:18:59 +0100

    basic server and client

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
-import socket
+# Basic implemintation of web server and client as show o nhttps://pandeyshikha075.medium.com/building-a-chat-server-and-client-in-python-with-socket-programming-c76de52cc1d5
+# This structure was closely followed and will be modified to fit the coursework
+import socket 
 import threading
-
-MAX_DATA_RECV = 65535
-
-
-# Code to set up the client and server structure, 
-# https://pandeyshikha075.medium.com/building-a-chat-server-and-client-in-python-with-socket-programming-c76de52cc1d5
-# The following website was used to explain and set up the basic structure
+ 
+# Handle the request sent to by the client to the server
+def handleRequest(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        message = data.decode('utf-8')
+        print(f"Received message: {message}")
+        response = "Server received your message: " + message
+        client_socket.sendall(response.encode('utf-8'))
+    client_socket.close()
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '127.0.0.1'
-    port = 12345
+    port = 12344
     server_socket.bind((host, port))
     server_socket.listen(5)
     print(f"Server listening on {host}:{port}")
@@ -24,14 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    def handleRequest(clientsoc):
-        while True:
-            client_data = clientsoc.recieve(1024)
-            if not client_data:
-                break
-            client_message = client_data.deocde("utf-8")
-            print("Recieved Message: ", client_message)
-            response = "Server received your message: " + client_message
-            clientsoc.sendall(response.encode('utf-8'))
-        clientsoc.close()
