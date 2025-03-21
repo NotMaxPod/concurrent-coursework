a551c286c11dd1fa619ceb8f37c9b038d7307492
NotMaxPod <podolnymaxim@gmail.com>
2025-03-21 07:40:15 +0100

    Implimented message encryption and created classes inside the client and server file.

--- client.py
+++ client.py
@@ -5,23 +5,56 @@ import threading
 import socket 
 import bcrypt
 import tkinter as tk
- 
-def sendRequest(username):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
-    port = 12345
-    client_socket.connect((host, port))
 
-    while True:
-        message = input("Enter your message: ")
-        newmessage = username + "usersplit" + message
-        client_socket.sendall(newmessage.encode('utf-8'))
-        if message == "/terminate":
-            client_socket.close()
-            break
-        data = client_socket.recv(1024)
-        response = data.decode('utf-8')
-        print(f"Server response: {response}")
+class Client():
+ 
+    def sendRequest(self, username):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = '127.0.0.1'
+        port = 12345
+        client_socket.connect((host, port))
+
+        while True:
+            message = input("Enter your message: ")
+            newmessage = username + "usersplit" + message
+            client_socket.sendall(newmessage.encode('utf-8'))
+            if message == "/exit":
+                client_socket.close()
+                break
+            data = client_socket.recv(1024)
+            response = data.decode('utf-8')
+            print(f"Server response: {response}")
+
+    def registerUser(self, username,password):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+
+        c.execute('''CREATE TABLE IF NOT EXISTS users
+                    (username text, password text)''')
+        
+        hashed_password = hashPass(password)
+
+        c.execute("INSERT INTO users VALUES (?, ?)", (username, hashed_password))
+
+        conn.commit()
+        conn.close()
+
+    def loginUser(self, username, password):
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+
+        c.execute("SELECT password FROM users WHERE username = ?", (username,))
+        hashed_password = c.fetchone()
+
+        logged_in = False
+        if hashed_password is None:
+            conn.close()
+            return logged_in
+        if verifyPass(password, hashed_password[0]):
+            logged_in = True
+        
+        conn.close()
+        return logged_in
 
 def hashPass(password):
     salt = bcrypt.gensalt()
@@ -30,50 +63,20 @@ def hashPass(password):
 def verifyPass(password, hashed_password):
     return bcrypt.checkpw(password.encode(), hashed_password)
 
-def registerUser(username,password):
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS users
-                 (username text, password text)''')
-    
-    hashed_password = hashPass(password)
-
-    c.execute("INSERT INTO users VALUES (?, ?)", (username, hashed_password))
-
-    conn.commit()
-    conn.close()
-
-def loginUser(username, password):
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-
-    c.execute("SELECT password FROM users WHERE username = ?", (username,))
-    hashed_password = c.fetchone()
-
-    logged_in = False
-    if hashed_password is None:
-        conn.close()
-        return logged_in
-    if verifyPass(password, hashed_password[0]):
-        logged_in = True
-    
-    conn.close()
-    return logged_in
-
 if __name__ == "__main__":
+    new_client = Client()
 
     while True:
         userinput = input("Would you like to register (r) or login (l): ")
         if userinput == "r":
             username = input("Enter your username: ")
             password = input("Enter your password: ")
-            registerUser(username, password)
+            new_client.registerUser(username, password)
 
         elif userinput == "l":
             username = input("Enter your username: ")
             password = input("Enter your password: ")
 
-            if loginUser(username, password):
+            if new_client.loginUser(username, password):
                 break
-    sendRequest(username)
+    new_client.sendRequest(username)
--- server.py
+++ server.py
@@ -1,35 +1,61 @@
 # Basic implemintation of web server and client as show o nhttps://pandeyshikha075.medium.com/building-a-chat-server-and-client-in-python-with-socket-programming-c76de52cc1d5
 # This structure was closely followed and will be modified to fit the coursework
 import socket 
+import sqlite3
 import threading
 import datetime
+from cryptography.fernet import Fernet
 
 lim = threading.Semaphore(3)
- 
-# Handle the request sent to by the client to the server
-def handleRequest(client_socket):
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        message = data.decode('utf-8')
-        username, message = message.split('usersplit')
-        if message == "/terminate":
-            break
-        current_time = datetime.datetime.now()
-        hour = current_time.hour
-        if hour < 10:
-            hour = '0' + str(hour)
-        minute = current_time.minute
-        if minute < 10:
-            minute = '0' + str(minute)
-        print(f"{username} {current_time.year}/{current_time.month}/{current_time.day} {hour}:{minute}: {message}")
-        response = "Server received your message: " + message
-        client_socket.sendall(response.encode('utf-8'))
-    lim.release()
-    client_socket.close()
+
+class Server():
+    def __init__(self):
+        # Key generate with Fernet.generate_key()
+        self.key = b'qcF8OL2wPTO7Ic7Wr3pUhwjRfdVf4YDJmgzf57IGRWs='
+    # Handle the request sent to by the client to the server
+    def handleRequest(self, client_socket):
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            message = data.decode('utf-8')
+            username, message = message.split('usersplit')
+            if message == "/exit":
+                break
+            self.encryptToDb(username, message)
+            current_time = datetime.datetime.now()
+            hour = current_time.hour
+            if hour < 10:
+                hour = '0' + str(hour)
+            minute = current_time.minute
+            if minute < 10:
+                minute = '0' + str(minute)
+            print(f"{username} {current_time.year}/{current_time.month}/{current_time.day} {hour}:{minute}: {message}")
+            response = "Server received your message: " + message
+            client_socket.sendall(response.encode('utf-8'))
+        lim.release()
+        client_socket.close()
+
+    def encryptToDb(self, username, message):
+
+        fer = Fernet(self.key)
+
+        encrypted = fer.encrypt(message.encode('utf-8'))
+
+        conn = sqlite3.connect('messages.db')
+        c = conn.cursor()
+
+        c.execute('''CREATE TABLE IF NOT EXISTS messages
+                 (username text, message text)''')
+        
+        c.execute("INSERT INTO messages VALUES (?, ?)", (username, encrypted))
+
+        conn.commit()
+        conn.close()
+
 
 def main():
+    theServer = Server()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '127.0.0.1'
     port = 12345
@@ -40,7 +66,7 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Accepted connection from {client_address}")
-        client_handler = threading.Thread(target=handleRequest, args=(client_socket,))
+        client_handler = threading.Thread(target=theServer.handleRequest, args=(client_socket,))
         lim.acquire()
         client_handler.start()
 
