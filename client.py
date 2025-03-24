256fa603028083278e67d0d6430ae6db41beee93
NotMaxPod <podolnymaxim@gmail.com>
2025-03-24 10:03:35 +0100

    Added proper comments to the code and formatted using black.

--- client.py
+++ client.py
@@ -1,28 +1,31 @@
-# Python program to implement client side of chat room. 
+# Python program to implement client side of chat room.
 
 import sqlite3
-import socket 
+import socket
 import bcrypt
-from tkinter import*
+from tkinter import *
 from threading import *
 import tkinter as tk
 import time
 
-class Client():
 
+# Definition of the client class
+class Client:
+    # Username and password
     def __init__(self):
         self.username = ""
         self.password = ""
-    
-    
-    
+        self.mute = False
+
+    # Main function of the class handle request which processes the request before sending it to server
     def sendRequest(self, username):
-            
+        # Connect to server on the designated address
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = '127.0.0.1'
+        host = "127.0.0.1"
         port = 12345
         client_socket.connect((host, port))
-        
+
+        # User display window
         messageWindow = tk.Tk()
         messageWindow.title("Message Interface of " + username)
 
@@ -33,53 +36,58 @@ class Client():
         responseField.pack(pady=10)
         responseField.config(state="disabled")
 
-        
-        
+        # Function to send the message to user with designated messqage divider for the server to separate the username and the message
         def sendMessage():
             message = messageEntry.get("1.0", "end-1c")
             newmessage = username + "usersplit" + message
-            client_socket.sendall(newmessage.encode('utf-8'))
+            client_socket.sendall(newmessage.encode("utf-8"))
             messageEntry.delete("1.0", "end")
 
+        # Basic function for recieving server replies and messages, only broadcasting them to the user if mute == False
         def receiveMessage():
             data = client_socket.recv(1024)
-            response = data.decode('utf-8')
-            responseField.config(state="normal")
-            responseField.insert("end", response + "\n")
-            responseField.config(state="disabled")
+            if self.mute == False:
+                response = data.decode("utf-8")
+                responseField.config(state="normal")
+                responseField.insert("end", response + "\n")
+                responseField.config(state="disabled")
 
+        # Refreshing thread for constantly reciving responses from the server independentatly of sending
         def refresh():
             while True:
                 receiveMessage()
-                time.sleep(1)
+                time.sleep(3)
 
         updater = Thread(target=refresh)
         updater.start()
-        
+
+        # Send message button
         sendButton = tk.Button(messageWindow, text="Send", command=sendMessage)
         sendButton.pack(pady=10)
 
-        # Create a file send button
+        # File sending function, transfering the file in parts to the server
         def sendFile():
             fileName = fileEntry.get()
             check = fileName.split(".")
+            # Prepare the message for the server to switch to file processing mode on the server side.
             serverMessage = username + "usersplit" + "/send"
             if check[1] == "docx" or check[1] == "pdf" or check[1] == "jpeg":
-                client_socket.send(serverMessage.encode('utf-8'))
+                client_socket.send(serverMessage.encode("utf-8"))
                 client_socket.send(fileName.encode())
                 with open(fileName, "rb") as file:
                     fileData = file.read()
                     client_socket.send(fileData)
                     time.sleep(1)
+                    # A final snippet of data to indciate that the entrie file has been sent for the server
                     client_socket.sendall(b"Finished transfering.")
-            
 
             else:
-                client_socket.send(serverMessage.encode('utf-8'))
+                # Bad file type error handling
+                client_socket.send(serverMessage.encode("utf-8"))
                 fileName = "File type not supported."
-                client_socket.send(fileName.encode('utf-8'))
-                #print("File type not supported.")
+                client_socket.send(fileName.encode("utf-8"))
 
+        # File sending buttons and labels for the display
         fileLabel = tk.Label(messageWindow, text="File Name:")
         fileLabel.pack()
         fileEntry = tk.Entry(messageWindow)
@@ -87,58 +95,39 @@ class Client():
         fileButton = tk.Button(messageWindow, text="Send File", command=sendFile)
         fileButton.pack(pady=10)
 
-        # Create an exit button
+        # Toggles server responses being sent to user, including notifications of other users sending messages
+        def toggleMute():
+            self.mute = not self.mute
+            if self.mute:
+                muteButton.config(text="Unmute")
+            else:
+                muteButton.config(text="Mute")
+
+        muteButton = tk.Button(messageWindow, text="Mute", command=toggleMute)
+        muteButton.pack(pady=10)
+
+        # Exit button for the client, allowing them to disconnect from the server and advance the server que.
         def exitClient():
-            client_socket.sendall((username + "usersplit" + "/exit").encode('utf-8'))
+            self.mute = False
+            client_socket.sendall((username + "usersplit" + "/exit").encode("utf-8"))
             client_socket.close()
             messageWindow.destroy()
 
         exitButton = tk.Button(messageWindow, text="Exit", command=exitClient)
         exitButton.pack(pady=10)
 
-        
-
         messageWindow.mainloop()
-        '''while True:
-            message = input("Enter your message: ")
-            newmessage = username + "usersplit" + message
-            if message == "/send":
-                client_socket.send(newmessage.encode('utf-8'))
-                fileName = input("Please enter the file name: ")
-                check = fileName.split(".")
-                if check[1] == "docx" or check[1] == "pdf" or check[1] == "jpeg":
-                    client_socket.send(fileName.encode())
-                    with open(fileName, "rb") as file:
-                        fileData = file.read()
-                        #print(fileData)
-                        client_socket.send(fileData)
-                        time.sleep(1)
-                        client_socket.sendall(b"Finished transfering.")
-                        continue
-                else:
-                    print("File type not supported.")
-                    continue
-                print("File sent")
-                continue
-            client_socket.sendall(newmessage.encode('utf-8'))
 
-            # Client termination if termibation command entered
-            if message == "/exit":
-                client_socket.close()
-                break
-            data = client_socket.recv(1024)
-            response = data.decode('utf-8')
-            if response == "Muted." or response == "Empty":
-                continue
-            print(f"Sent to server: {response}")'''
-
-    def registerUser(self, username,password):
-        conn = sqlite3.connect('users.db')
+    # Register method which takes the user inputs and writes them to the database after encrypting the password
+    def registerUser(self, username, password):
+        conn = sqlite3.connect("users.db")
         c = conn.cursor()
 
-        c.execute('''CREATE TABLE IF NOT EXISTS users
-                    (username text, password text)''')
-        
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS users
+                    (username text, password text)"""
+        )
+
         c.execute("SELECT * FROM users WHERE username = ?", (username,))
         if c.fetchone() is not None:
             print("Username already taken. Data not saved to database.")
@@ -149,8 +138,9 @@ class Client():
             conn.commit()
             conn.close()
 
+    # Login method which takes the user inputs and compares them to data stored on the database to verify the inputs
     def loginUser(self, username, password):
-        conn = sqlite3.connect('users.db')
+        conn = sqlite3.connect("users.db")
         c = conn.cursor()
 
         c.execute("SELECT password FROM users WHERE username = ?", (username,))
@@ -162,28 +152,39 @@ class Client():
             return logged_in
         if verifyPass(password, hashed_password[0]):
             logged_in = True
-        
+
         conn.close()
         return logged_in
 
+
+# Basic password hashing function
 def hashPass(password):
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
 
+
+# Basic comparator function to compare input password to hashed version
 def verifyPass(password, hashed_password):
     return bcrypt.checkpw(password.encode(), hashed_password)
 
+
+# User login display, promoting the user to either login or register a new account
 def loginDisplay():
     global new_client
+
+    # Display and label set up
     clientDisplay = tk.Tk()
     clientDisplay.title("Client Interface")
-    
-    greetingLable = Label(clientDisplay, text="Welcome to your chatting application!", fg="black")
+
+    greetingLable = Label(
+        clientDisplay, text="Welcome to your chatting application!", fg="black"
+    )
     greetingLable.pack(pady=20)
 
     buttonFrame = Frame(clientDisplay)
     buttonFrame.pack(pady=20)
 
+    # A secondary window using Toplevel in order for the user to input their credentials either for logging in or registering
     def userDataPrompt():
         userData = tk.Toplevel(clientDisplay)
         userData.title("Your login credentials")
@@ -198,29 +199,30 @@ def loginDisplay():
         passwordEntry = Entry(userData, show="*")
         passwordEntry.pack()
 
+        # Collect entries and destroy additional window
         def buttonPress():
             new_client.username = usernameEntry.get()
             new_client.password = passwordEntry.get()
             userData.quit()
             userData.destroy()
-            
 
-        
         enterButton = Button(userData, text="Enter Data", command=buttonPress)
         enterButton.pack()
         userData.mainloop()
 
-
+    # Register and login functions, potentially will be migrated to the server, current implemintation is sloely for the proof of encryption
+    # The genuine encryption of user data should be more secure but works in context of the coursework
     def registerButton():
         userDataPrompt()
         new_client.registerUser(new_client.username, new_client.password)
 
+    # After confirming data, initialise the first request
     def loginButton():
         userDataPrompt()
         if new_client.loginUser(new_client.username, new_client.password):
             clientDisplay.destroy()
             new_client.sendRequest(new_client.username)
-    
+
     # Create the login button
     login_button = Button(buttonFrame, text="Login", command=loginButton)
     login_button.pack(side=LEFT, padx=10)
@@ -236,5 +238,3 @@ if __name__ == "__main__":
     new_client = Client()
 
     loginDisplay()
-
-
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # Basic implemintation of web server and client as show o nhttps://pandeyshikha075.medium.com/building-a-chat-server-and-client-in-python-with-socket-programming-c76de52cc1d5
 # This structure was closely followed and will be modified to fit the coursework
-import socket 
+import socket
 import sqlite3
 import threading
 import datetime
@@ -11,135 +11,163 @@ from tkinter import *
 from cryptography.fernet import Fernet
 import sys
 
+# Que and Connected, storing the connections of all user who log into the server. After connections >= 3, all further connections are added to que.
 que = {}
 connected = {}
 
-class Server():
+
+# The server class
+class Server:
     def __init__(self):
-        # Key generate with Fernet.generate_key()
-        self.key = b'qcF8OL2wPTO7Ic7Wr3pUhwjRfdVf4YDJmgzf57IGRWs='
-        self.mute = False
+        # Key generated with Fernet.generate_key()
+        self.key = b"qcF8OL2wPTO7Ic7Wr3pUhwjRfdVf4YDJmgzf57IGRWs="
 
-    # Handle the request sent to by the client to the server
+    # Main functions of the server class for processing client requests sent by the user
     def handleRequest(self, client_socket, client_address):
-
+        # Main server loop for constantly recieving data from the user, break if empty
         while True:
-            #print(connected)
             data = client_socket.recv(1024)
             if not data:
                 break
-            message = data.decode('utf-8')
-            username, message = message.split('usersplit')
 
+            # Decode the message and separate username from user input
+            message = data.decode("utf-8")
+            username, message = message.split("usersplit")
+
+            # If message sent by the server is of the /send type, intiate file transfering
             if message == "/send":
+                # Flag for data reception and file name
                 doneSending = False
                 fileName = client_socket.recv(1024)
-                if fileName.decode('utf-8') == "File type not supported.":
+                if fileName.decode("utf-8") == "File type not supported.":
                     client_socket.sendall(fileName)
                     continue
-                fileName = username.encode('utf-8') + fileName
+                # Create new unique file name with the user's name to clearly indicate who sent it
+                fileName = username.encode("utf-8") + fileName
                 file = open(fileName, "wb")
                 newFileBytes = b""
+                # Loop for larger files being transfered, when the tag "Finished transfering." or empty bytes are sent, change flag and break loop
                 while not doneSending:
                     fileData = client_socket.recv(1024)
-                    if fileData == b"Finished transfering." or fileData == b'':
+                    if fileData == b"Finished transfering." or fileData == b"":
                         doneSending = True
                         break
                     else:
                         newFileBytes += fileData
+                # Encrypt the data of the file to database before writing it on the server
                 self.encryptToDb(username, fileData.decode(), "File.")
                 file.write(newFileBytes)
                 file.close()
+                # Some background server terminal outputs for debug purposes
                 print(username, "has sent a file to the server.")
-                client_socket.send("File sent to server.".encode('utf-8'))
+                # Send response to client
+                client_socket.send("File sent to server.".encode("utf-8"))
                 continue
 
+            # Call user managing function to refresh que
             response = self.manageUsers(client_socket, username, message)
-            
+
             # Client is not in queue, process message
+
+            # Exit message to terminate connection and update currenly connected users
             if message == "/exit":
                 # Remove client from connected clients
                 del connected[client_socket]
                 break
 
-            #Update current status of mute flag
-            elif message == "/mute":
-                if self.mute:
-                    self.mute = False
-                else:
-                    self.mute = True
-            if self.mute:
-                response = "Muted."
+            # The main message processing for basic texts sent in the chat
             elif client_socket in connected.keys():
+                # Store message history to database
                 self.encryptToDb(username, message, "Chat Message.")
+
+                # Organise current time to print to server.
                 current_time = datetime.datetime.now()
                 hour = current_time.hour
                 if hour < 10:
-                    hour = '0' + str(hour)
+                    hour = "0" + str(hour)
                 minute = current_time.minute
                 if minute < 10:
-                    minute = '0' + str(minute)
+                    minute = "0" + str(minute)
+
+                # Main message print statement
                 message = f"{username} {current_time.year}/{current_time.month}/{current_time.day} {hour}:{minute}: {message}"
                 print(message)
                 response = "Message sent to server."
 
+                # Send notification to all other users from the server that current client sent a message
                 for x in connected.keys():
-                    #print(client_socket)
                     if x != client_socket:
-                        #print(x)
                         clients = f"{username} sent a message!"
                         x.send(clients.encode())
 
+                # Update chat
                 self.chatText.insert(tk.END, message + "\n")
                 self.chatText.see(tk.END)
 
-    
-
-            client_socket.sendall(response.encode('utf-8'))
+            # Send response from server to client
+            client_socket.sendall(response.encode("utf-8"))
 
         client_socket.close()
 
+    # User managment method
     def manageUsers(self, client_socket, username, message):
+        # Set default response to empty, meaning the server returns no response
         response = "Empty"
+        # If connection is free and the current user isn't connected, add them to connection
         if (len(connected) < 3) and (client_socket not in connected.keys()):
-                connected[client_socket] = username
-                #print("Added to connected")
-            
+            connected[client_socket] = username
+            # print("Added to connected")
+
+        # The server is too full, add current client to que and store their connection time. If already in que, return waiting time as server response
         elif len(connected) > 2:
-            if (client_socket not in que.keys()) and (client_socket not in connected.keys()):
+            if (client_socket not in que.keys()) and (
+                client_socket not in connected.keys()
+            ):
                 que[client_socket] = time.time()
                 response = "The server is current at maximum capcity, you have been added to the que."
-                #print("Added to que.")
+                # print("Added to que.")
 
             elif client_socket in que.keys():
-                waitingTime = time.time() - que[client_socket]
+                waitingTime = round((time.time() - que[client_socket]), 2)
                 response = f"Waiting in queue for {waitingTime} seconds."
-                #print("in que")
-            
+                # print("in que")
+        # Connection has decreased and the user is in que, move user to connected
         elif len(connected) < 3 and client_socket in que.keys():
-                del que[client_socket]
-                connected[client_socket] = username
-                response = message
-                #print("removed from que")
+            del que[client_socket]
+            connected[client_socket] = username
+            response = message
+            # print("removed from que")
 
         return response
+
+    # Encryption of files and message history to a designated database
     def encryptToDb(self, username, message, message_type):
-
+        # Retrive encryption key used by the server and apply to message
         fer = Fernet(self.key)
+        encrypted = fer.encrypt(message.encode("utf-8"))
 
-        encrypted = fer.encrypt(message.encode('utf-8'))
-
-        conn = sqlite3.connect('messages.db')
+        # Open the database and add current message/file to it
+        conn = sqlite3.connect("messages.db")
         c = conn.cursor()
 
-        c.execute('''CREATE TABLE IF NOT EXISTS messages
-                 (username text, message text, message_type)''')
-        
-        c.execute("INSERT INTO messages VALUES (?, ?, ?)", (username, encrypted, message_type,))
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS messages
+                 (username text, message text, message_type)"""
+        )
+
+        c.execute(
+            "INSERT INTO messages VALUES (?, ?, ?)",
+            (
+                username,
+                encrypted,
+                message_type,
+            ),
+        )
 
         conn.commit()
         conn.close()
 
+    # Server chat window initalisation
     def serverDisplay(self):
         self.chatWindow = tk.Tk()
         self.chatWindow.title("Server Chat")
@@ -148,39 +176,38 @@ class Server():
 
         self.chatText.mainloop()
 
-
+    # Boot method to intialise a client thread
     def boot(self, server_socket):
         while True:
             client_socket, client_address = server_socket.accept()
             print(f"Accepted connection from {client_address}")
-            client_handler = threading.Thread(target=self.handleRequest, args=(client_socket,client_address,))
+            client_handler = threading.Thread(
+                target=self.handleRequest,
+                args=(
+                    client_socket,
+                    client_address,
+                ),
+            )
             client_handler.start()
 
 
 def main():
-
+    # Server initialisation in the main method
     theServer = Server()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'
+    host = "127.0.0.1"
     port = 12345
 
     server_socket.bind((host, port))
     server_socket.listen(5)
     print(f"Server listening on {host}:{port}")
 
+    # Seaparate server booting method due to tkinter not being able to run on non main threads.
     serverThread = threading.Thread(target=theServer.boot, args=(server_socket,))
     serverThread.start()
 
     theServer.serverDisplay()
 
-    while True:
-        
-        client_socket, client_address = server_socket.accept() 
-        print(f"Accepted connection from {client_address}")
-        clientThread = threading.Thread(target=theServer.handleRequest, args=(client_socket,client_address,))
-        clientThread.start()
-
 
 if __name__ == "__main__":
     main()
-
